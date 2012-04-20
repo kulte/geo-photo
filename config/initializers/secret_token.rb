f88b4fb0c8590aac6884564bb02c296435144690
Zachary Friedman <zafriedman@gmail.com>
2012-04-20 17:33:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoPhoto::Application.config.secret_token = 'a1a27b67b7c189db201062ab665a202d51cf747ffc993a87c814d9e65506c5b4873f2c1e68b07f6d94b00ff79b0a8332aaeb0ad4869e1c4e1152dae1f55bb453'
